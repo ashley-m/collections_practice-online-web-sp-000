e7275204bb13760e98a7b86fa9b96593c1dac417
Ashley Miller <asm2fe@virginia.edu>
2019-04-02 19:30:01 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -9,3 +9,10 @@ def sort_array_desc(collection)
   sorted = sort_array_asc(collection).reverse
   return sorted
 end
+
+def sort_array_char_count(collection)
+  # lengths = []
+  # collection.each_with_index {|x, i| lengths[i] = x.length}
+  # lengths.each_with_index {|l, i| }
+  collection.sort
+end
